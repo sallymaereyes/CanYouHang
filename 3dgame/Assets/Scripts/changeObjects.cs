2fd4838e6a7698e30bb7a96e9714351b4211290e
timphimmas <tphimmasone@sdsu.edu>
2017-12-08 18:43:01 -0800

    Added Wineglass to spawn area

---
+++ 3dgame/Assets/Scripts/changeObjects.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeObjects : MonoBehaviour {
+
+    public GameObject golfball;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Instantiate(golfball, transform.position, transform.rotation);
+            Debug.Log("change object");
+        }
+    }
+}
