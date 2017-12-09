cfebab81150f2a6bb4811713cdc5c20cdc48daa9
Victoria Huynh <vickyhahuynh@yahoo.com>
2017-12-09 13:30:28 -0800

    objects switch randomly and collide with target and disappears on collision

---
+++ Assets/Scripts/cylinderCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cylinderCollision : MonoBehaviour {
+    private void Start()
+    {
+    }
+   
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("hit");
+
+        if (collision.gameObject.tag == "square")
+        {
+            Destroy(collision.gameObject);
+            Debug.Log("cylinder collision hit");
+        }
+    }
+}
